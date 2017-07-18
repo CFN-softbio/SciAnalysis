b30edc1a6b7115886c74fad093f7398f0ddc3fee
Julien L <jrmlhermitte@gmail.com>
2017-07-18 16:49:23 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# SciAnalysis for CMS pipeline.
+
+## Author : Kevin G. Yager
+## Ported to git by : Julien Lhermitte
+
+SciAnalysis is a set of Python scripts for batch processing of image data,
+including x-ray scattering detector images. The code was written primarily by
+Kevin Yager.
+
+http://gisaxs.com/index.php/SciAnalysis
+
+INSTALL:
+
+ * python setup.py develop
+
+TODO:
+
+ * current tests are not unit tests. Meant only for debugging
+ * add databroker interface to all functions
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import numpy as np
+
+# from distutils.core import setup
+import setuptools
+
+setuptools.setup(name='SciAnalysis',
+                 version='1.0',
+                 author='Kevin Yager',
+                 description="CMS Analysis",
+                 include_dirs=[np.get_include()],
+                 author_email='kyager@bnl.gov',
+                 # install_requires=['six', 'numpy'],  # essential deps only
+                 keywords='CMS X-ray Analysis',
+                 )
