ebe8d451e4d584bbef82f3fa83b01e9e60b73587
Kevin Yager <kevin.yager@gmail.com>
2023-01-27 13:39:35 -0500

    PyPI

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(path.join(here, "requirements.txt")) as requirements_file:
 
 
 setup(
-    name="SciTools-SciAnalysis",
+    name="SciToolsSciAnalysis",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="SciAnalysis scripts for processing image files.",
