3df9d7d68171502d10e76cd5eccb6e9abfd3b2c7
CMS Operator <xf11bm@xf11bm-ws5.nsls2.bnl.local>
2023-09-15 16:03:18 -0400

    Minor plot change on log-log

--- SciAnalysis/ExpAnalysis/Tools.py
+++ SciAnalysis/ExpAnalysis/Tools.py
@@ -48,44 +48,50 @@ def rand_color(a, b):
 # =============================================================================
 # Load a Line and find/return peaks
 # =============================================================================
-def plot_peaks(line, N_peaks_find=2, fit_param = [0,1,0.001], verbose=0, line_color = 'k', label_color='r', flag_log=1, roundup=3, gridline=True):
+def plot_peaks(line, N_peaks_find=2, fit_param = [0,1,0.001], verbose=0, line_color = 'k', label_color='r', flag_log=[1, 0], roundup=3, gridline=True):
     if verbose>0:
         print('fit_param = [height, width, prominence]')
 
-    if flag_log==1:
+    if flag_log[0]==1:
         line_y = np.log(line.y)
     else:
         line_y = line.y
 
-    plt.plot(line.x, line_y, color = line_color); 
+    if flag_log[1] == 1:
+        line_x = np.log(line.x)
+    else:
+        line_x = line.x
+
+    plt.plot(line_x, line_y, color = line_color); 
     peaks, _ = find_peaks(line_y, height=fit_param[0], width=fit_param[1], prominence=(fit_param[2], None))
     while len(peaks)>N_peaks_find:
         #print('  N_peaks = {}, increase fit_prom to reduce N_peaks'.format(len(peaks)))
         fit_param[2] = fit_param[2]*1.01
         peaks, _ = find_peaks(line_y, height=fit_param[0], width=fit_param[1], prominence=(fit_param[2], None)) 
-    print('{} peaks found: {}'.format(len(peaks), np.round(line.x[peaks],4)) +' for fit_prom {:.5f}'.format(fit_param[2]))
+    print('{} peaks found: {}'.format(len(peaks), np.round(line_x[peaks],4)) +' for fit_prom {:.5f}'.format(fit_param[2]))
     
     ylim = [np.nanmin(line_y[line_y != -np.inf]), np.nanmax(line_y)]
     yrange = ylim[1]-ylim[0]
     for idx, peak in enumerate(peaks):
-        plt.plot([line.x[peak], line.x[peak]], ylim, '--', color=label_color)
+        plt.plot([line_x[peak], line_x[peak]], ylim, '--', color=label_color)
         if verbose > 0:
-            plt.text(line.x[peak], ylim[1]-np.mod(idx,4)*yrange*0.04, str(np.round(line.x[peak],roundup)),fontweight='bold')
+            plt.text(line_x[peak], ylim[1]-np.mod(idx,4)*yrange*0.04, str(np.round(line_x[peak],roundup)),fontweight='bold')
     if gridline:
         plt.grid(True, which='major', color='k', linestyle='-', alpha=0.3) 
     
-    return line.x[peaks]
+    return line_x[peaks]
 
 
 # =============================================================================
 # Label peaks
 # =============================================================================
-def label_peaks(q_labels, flag_log=1, y_range=[0, 1], color='g', roundup=3, verbose=1):
+def label_peaks(q_labels, flag_log=[1, 0], y_range=[0, 1], color='g', roundup=3, verbose=1):
     for idx, q in enumerate(q_labels):
-        if flag_log:
+        if flag_log[0]==1:
             plot_range = [np.log(np.max([1, y_range[0]])), np.log(y_range[1])]
         else:
             plot_range = [y_range[0], y_range[1]]
+            
         if math.isnan(q)==False:
             plt.plot([q, q], plot_range, color=color,  alpha=0.5)
             if verbose>0:
